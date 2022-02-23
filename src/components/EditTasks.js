a1f24ec0c096de9c13a131db10cee45f0c754d45
Ehylin <ehylinolezjua@.com>
2022-02-23 20:55:01 -0300

    creando componentes tareas y editar tarea

--- src/components/EditTasks.js
+++ src/components/EditTasks.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const EditTasks = () => {
 return(
-    <h1>desde editar tarea nueva</h1>
+    <div className='row justify-content-center'>
+    <div className='col-md-8'>
+        <div className='card'>
+            <div className='card-body'>
+                <h2 className='text-center mb-4 font-weight-bold'>
+                        Editar Tarea
+                </h2>
+
+                <form>
+                    <div className='form-group'>
+                        <label>Nombre tarea</label>
+                        <input
+                        type='text'
+                        className='form-control'
+                        placeholder='Nombre tarea' 
+                        name='name'
+                    />
+                    </div>
+                    <button
+                    type='submit'
+                    className='btn btn-primary font-weight-bold text-uppercase d-block w-100'>
+                        Guardar Cambios
+                    </button>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+</div>
 )
 }
 
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
 const Tasks = () => {
     return (
-        <h1>desde tareas</h1>
+        <Fragment>
+            <h2 className='text-center my-5'>Listado de tareas</h2>
+
+            <table className='table table-striped'>
+                <thead className='bg-primary table-dark'>
+                    <tr>
+                        <th scope='col'>Nombre</th>
+                        <th scope='col'>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    
+                </tbody>
+            </table>
+        </Fragment>
     )
 }
 
