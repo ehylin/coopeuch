4706f080566dd9b716b4e9d5e1311cd08773b0f9
Ehylin <ehylinolezjua@.com>
2022-02-23 20:47:18 -0300

    creando formulario para crear una nueva tarea

--- src/components/NewTasks.js
+++ src/components/NewTasks.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const NewTasks = () => {
 return(
-    <h1>desde crear tarea nueva</h1>
+    <div className='row justify-content-center'>
+        <div className='col-md-8'>
+            <div className='card'>
+                <div className='card-body'>
+                    <h2 className='text-center mb-4 font-weight-bold'>
+                            Agregar nueva tarea
+                    </h2>
+
+                    <form>
+                        <div className='form-group'>
+                            <label>Nombre tarea</label>
+                            <input
+                            type='text'
+                            className='form-control'
+                            placeholder='Nombre tarea' 
+                            name='name'
+                        />
+                        </div>
+                        <button
+                        type='submit'
+                        className='btn btn-primary font-weight-bold text-uppercase d-block w-100'>
+                            Agregar
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 )
 }
 
